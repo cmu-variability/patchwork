77a020f3e0ac90d1861ef24ff955cbd65ce44d08
Kaia Newman <klnewman@andrew.cmu.edu>
2025-02-06 11:16:22 -0500

    Added chart12 for testing patch-applying plugin.

---
+++ test.java.backup
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        int x = 6;
+        String y = "bananas";
+        System.out.println(y);
+    }
+    
+}
---
+++ test2.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Claire!");
+        int x = 6;
+        String y = "mangos";
+        System.out.println(y);
+        int z = 5;
+    }
+    
+}
